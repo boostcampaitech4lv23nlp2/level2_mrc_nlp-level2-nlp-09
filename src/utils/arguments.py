6fb00207edc9f2d492deb55a6755dd90cafc84c1
jinmyeongAN <jinmyeong.an@gmail.com>
2022-12-21 12:22:56 +0000

    fix     : change the path of config and data

--- src/utils/arguments.py
+++ src/utils/arguments.py
@@ -6,7 +6,7 @@ from transformers import TrainingArguments
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../config/config.yml') as f:
+with open('./src/config/config.yml') as f:
     data = yaml.load(f, Loader=SafeLoader)
 
 def get_training_args(
