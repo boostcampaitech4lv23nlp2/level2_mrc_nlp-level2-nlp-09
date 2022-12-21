d25033ef223c1260cfd76f7b98e3f34dd679c9a8
jinmyeongAN <jinmyeong.an@gmail.com>
2022-12-21 13:01:29 +0000

    feat    : seperate training_args with other args * because training_arg is from transformers module not from custom dataclass

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from transformers import HfArgumentParser, TrainingArguments
 
 from src import inference, train
-from src.utils import DataTrainingArguments, ModelArguments
+from src.utils import DataTrainingArguments, ModelArguments, get_training_args
 
 if __name__ == "__main__":
 
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
-
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments))
+    model_args, data_args = parser.parse_args_into_dataclasses()
+    training_args = get_training_args()
+    
     if training_args.do_train:
         train(model_args, data_args, training_args)
     if training_args.do_predict:
