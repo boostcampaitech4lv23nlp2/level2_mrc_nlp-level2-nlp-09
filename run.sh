931e873806744a977486ceba4a39afda1586c592
Yongwoo Song <ywsong.dev@kakao.com>
2022-12-22 01:47:42 +0000

    feat: add roberta baseline

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
-do_train="True"
-do_eval="True"
-do_predict="False"
+do_train="False"
+do_eval="False"
+do_predict="True"
 output_dir="output"
 max_seq_length="384"
 pad_to_max_length="False"
 learning_rate="5e-05"
-num_train_epochs="1"
-per_device_train_batch_size="8"
-per_device_eval_batch_size="8"
-model_name_or_path="klue/bert-base"
+num_train_epochs="5"
+per_device_train_batch_size="16"
+per_device_eval_batch_size="16"
+model_name_or_path="klue/roberta-large"
+fp16="True"
 
 python main.py \
     --do_eval ${do_eval} \
